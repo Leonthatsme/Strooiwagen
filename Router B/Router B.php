a6c3f5f9ce90144b3a4d6840ccce49e1296697f8
Leon <Leonnwlnd@gmail.com>
2026-01-27 22:13:08 +0100

    Opdracht 5 Controllers

---
+++ Router B/Router B.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+use GuzzleHttp\Psr7\ServerRequest;
+use GuzzleHttp\Psr7\Response;
+use GuzzleHttp\Psr7\Utils;
+use HttpSoft\Emitter\SapiEmitter;
+use League\Route\Router;
+
+ini_set('display_errors', 1);
+
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+$request = ServerRequest::fromGlobals();
+
+$routerB = new Router;
+
+$routerB->map("GET", "/", function () {
+
+$stream = Utils::streamFor("Homepage");
+
+$response = new Response;
+
+$response = $response->withBody($stream);
+
+return $response;
+});
+
+$routerB->get("/products", function () {
+
+    $stream = Utils::streamFor("List of products");
+
+    $response = new Response;
+
+    $response = $response->withBody($stream);
+
+    return $response;
+});
+
+$routerB->get("/product/{id:number}", function ($request, $args) {
+
+    $id = $args["id"];
+
+    $stream = Utils::streamFor("Single product with ID $id");
+
+    $response = new Response;
+
+    $response = $response->withBody($stream);
+
+    return $response;
+});
+
+$response = $routerB->dispatch($request);
+
+$emitter = new SapiEmitter;
+
+$emitter->emit($response);
