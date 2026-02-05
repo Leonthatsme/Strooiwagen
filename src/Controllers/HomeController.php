74ddfc57b1b5bff5d0cc81fb22315833bf554888
Leon <Leonnwlnd@gmail.com>
2026-02-05 09:43:56 +0100

    Opdracht 7 Http Factory

--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -8,14 +8,20 @@ use GuzzleHttp\Psr7\Utils;
 use GuzzleHttp\Psr7\Response as GuzzleResponse;
 use Nyholm\Psr7\Response as NyholmResponse;
 use Psr\Http\Message\ResponseInterface;
+use Nyholm\Psr7\Stream;
+use GuzzleHttp\Psr7\HttpFactory;
+use Nyholm\Psr7\Factory\Psr17Factory;
 
 class HomeController
 {
     public function index(): ResponseInterface
     {
-        $stream = Utils::streamFor("Homepage");
+        $factory = new Psr17Factory();
 
-        $response = new GuzzleResponse;
+        $stream = $factory->createStream("Homepage");
+
+        $response = $factory->createResponse(200);
+
         $response = $response->withBody($stream);
 
         return $response;
